5405104caf978621837a28f7181879f201923df0
Yangsb <ysb5397@gmail.com>
2025-05-22 16:17:08 +0900

    temporary

--- Recruit_sys/src/swing/UserCreateFrame.java
+++ Recruit_sys/src/swing/UserCreateFrame.java
@@ -113,8 +113,7 @@ public class UserCreateFrame extends JFrame implements ActionListener {
             String pw = textFields[1].getText();
             String name = textFields[2].getText();
             String addr = textFields[3].getText();
-            User user = new User(0,name,email,pw,addr);
-            recruitSystemService.addUser(user);
+            recruitSystemService.addUser(name, email, pw, addr);
 
             this.dispose();
         } else if (targetB == idCheckButton) {
