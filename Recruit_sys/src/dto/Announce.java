6a942a72a62a8f8cb522fb92afb9b5398359a63f
Yangsb <ysb5397@gmail.com>
2025-05-22 14:24:22 +0900

    announce 컬럼 변경

--- Recruit_sys/src/dto/Announce.java
+++ Recruit_sys/src/dto/Announce.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Announce {
     private int id;
+    private int user_id;
+    private int company_id;
     private String company_name;
     private String address;
     private String content;
